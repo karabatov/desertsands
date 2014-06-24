c9d65de46de5dff5ef525dc97e04e16464bab621
Yuri Karabatov <yuri@yurikarabatov.com>
2014-06-24 21:49:47 +0400

    Initial Commit

---
+++ Desert SandsTests/Desert_SandsTests.swift
@@ -0,0 +1,35 @@
+//
+//  Desert_SandsTests.swift
+//  Desert SandsTests
+//
+//  Created by Yuri Karabatov on 24.06.14.
+//  Copyright (c) 2014 Yuri Karabatov. All rights reserved.
+//
+
+import XCTest
+
+class Desert_SandsTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
